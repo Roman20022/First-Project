2975df282d950e8ca838f3e78a3d0ae81690b220
Roman20022 <doshiklv@gmail.com>
2022-05-08 14:03:15 +0300

    For my teacher)

---
+++ main.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+t = tk.Tk()
+g = tk.Label(text="This is for my favorite teacher"
+                  "Hey, thank yo for teaching us interesting themes."
+                  "Good luck, amigo)")
+t.mainloop()
