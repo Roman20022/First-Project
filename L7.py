46ec5b0fb08f4f0b8185d15ce4a218c4c2c79eea
Roman20022 <doshiklv@gmail.com>
2022-05-24 21:24:19 +0300

    Lesson 7 24.05 22:00

---
+++ L7.py
@@ -0,0 +1,27 @@
+class Students:
+    grate = 12
+    def write(self):
+        pass
+class Marianna(Students):
+    writeOn12 = True
+    def Cheat(self):
+        pass
+class Denis(Students):
+    writeOn12 = False
+    grate = 2
+    def StudyBad(self):
+        pass
+class Andriy(Students):
+    writeOn12 = False
+    grate = 9
+    def StudyNotBad(self):
+        pass
+class Oleksandra(Students):
+    writeOn12 = False
+    grate = 11
+    def GotReadyToWork(self):
+        pass
+class Oleksandra(Students):
+    writeOn12 = True
+    def BeSoLucky(self):
+        pass
