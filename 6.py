fe35ec1a66ec7c2bfcbfd29bfa172485dfad1203
Roman20022 <doshiklv@gmail.com>
2022-05-28 10:05:59 +0300

    Lesson ??? 28.05 09:50 2022 encapsulation

--- 6.py
+++ 6.py
@@ -1,15 +1,26 @@
 class Client:
     def __init__(self,name,balans,creditBalans,passport):
-        self.name = name
-        self.balansOwnFunds = balans
-        self.balansCreditFunds = creditBalans
-        self.passport = passport
+        self.__name = name
+        self.__balansOwnFunds = balans
+        self.__balansCreditFunds = creditBalans
+        self.__passport = passport
     def addOwnFunds(self, money):
         self.balansOwnFunds =+ money
     def addCreditFunds(self, money):
         self.balansCreditFunds =+ money
-account1 = Client("Bob",1000,300,8970665028)
-print(account1.name)
-print(account1.balansOwnFunds)
-print(account1.balansCreditFunds)
-print(account1.passport)
+    # def printProtecrionData(self):
+    #     print(self._name, self._balansOwnFunds,self._balansCreditFunds,self._passport)
+
+
+
+    def __printPrivatData(self):
+        print(self.__name, self.__balansOwnFunds,self.__balansCreditFunds,self.__passport)
+
+account1 = Client("Bob", 1000, 300, 8970665028)
+account1.addOwnFunds(1000)
+account1._Client__printPrivatData()
+# print.printProtecrionData()
+# print(account1.__name)
+# print(account1.__balansOwnFunds)
+# print(account1.__balansCreditFunds)
+# print(account1.__passport)
