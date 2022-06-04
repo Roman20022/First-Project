4f1ea73e6949d952587238d43160fca3465b9a8e
Roman20022 <doshiklv@gmail.com>
2022-06-04 11:42:07 +0300

    L10 04.06 11:21 2022 Parser Response.content post data

--- Cw10.py
+++ Cw10.py
@@ -1,4 +1,8 @@
-import urllib.request
-opener = urllib.request.build_opener()
-response = opener.open("https://httpbin.org/get")
-print(response.read())
+# import urllib.request
+# opener = urllib.request.build_opener()
+# response = opener.open("https://httpbin.org/post")
+# print(response.read())
+import requests
+response= requests.post("https://httpbin.org/post", data="Test Data", headers={"h1":"test Title"})
+
+print(response.text)
